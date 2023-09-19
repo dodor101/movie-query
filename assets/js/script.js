1ee9ee2e9943ef9af9c67c2b09be8a443f4b2cb5
Dukenson Dodor <104813218+dodor101@users.noreply.github.com>
2023-09-19 07:34:17 -0500

    Cleaned codebase

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-const getMoveDetailsBtn = $('#get-movie-detains');
 const movieInputEl = $('#movie-title');
 const movieFormEl = $('#movie-form');
 const displayMovieInfo = $('#display-movie-info');
@@ -6,13 +5,16 @@ const displayMovieInfo = $('#display-movie-info');
 async function getMovieInfo(title) {
   try {
     const APIkey = `98d87208`;
+    if (!title) {
+      return alert('Enter a Movie Title');
+    }
 
     const url = `http://www.omdbapi.com/?apikey=${APIkey}&t=${title}`;
     const getData = await $.ajax({
       url: url,
       method: 'GET',
     });
-    displayMovieInfo.empty()
+    displayMovieInfo.empty();
     printMovieInfo(getData);
   } catch (error) {
     console.log(error);
@@ -20,20 +22,17 @@ async function getMovieInfo(title) {
 }
 
 function printMovieInfo(data) {
-  console.log(data)
   const { Title, Rated, Released, Runtime, Genre, Poster } = data;
   // console.log(Title, Rated, Released, Runtime, Genre, Poster);
-  const movieInfo = `
-<h3 class="text-center text-3xl">${Title}</h3>
+  const movieInfo =
+    `<h3 class="text-center text-3xl">${Title}</h3>
           <img class="h-96" src=${Poster}  />
-          <div>
-          <p>Released Date: ${Released}</p>
-           <p>Rated: ${Rated}</p>
-            <p>Runtime: ${Runtime}</p>
-             <p>Genre: ${Genre}</p>
-          </div>
-
-
+          <div class=" flex items-start flex-col">
+          <p> <span class="font-semibold">Released Date:</span>  ${Released}</p>
+           <p><span class="font-semibold">Rated:</span> ${Rated}</p>
+            <p><span class="font-semibold">Runtime:</span> ${Runtime}</p>
+             <p><span class="font-semibold">Genre:</span> ${Genre}</p>
+         </div>
 `;
   displayMovieInfo.append(movieInfo);
 }
